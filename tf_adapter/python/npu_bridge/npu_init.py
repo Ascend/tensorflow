a21f9a4ea9c763070f7a9d295bfb1a507d4d1cdb
yanqingshang <yanqingshang@huawei.com>
2020-12-11 15:53:49 +0800

    update npu_init.py

--- tf_adapter/python/npu_bridge/npu_init.py
+++ tf_adapter/python/npu_bridge/npu_init.py
@@ -1,10 +1,43 @@
 from npu_bridge.estimator.npu.npu_config import NPURunConfig
+from npu_bridge.estimator.npu.npu_config import ProfilingConfig
+from npu_bridge.estimator.npu.npu_config import DumpConfig
+from npu_bridge.estimator.npu.npu_config import DynamicInputConfig
+
 from npu_bridge.estimator.npu.npu_estimator import NPUEstimator
 from npu_bridge.estimator.npu.npu_optimizer import NPUDistributedOptimizer
+
+from npu_bridge.estimator.npu.npu_hook import NPUCheckpointSaverHook
+from npu_bridge.estimator.npu.npu_hook import NPUOutputTensorHook
+
+from npu_bridge.estimator.npu.npu_optimizer import NPUDistributedOptimizer
+from npu_bridge.estimator.npu.npu_optimizer import NPUOptimizer
+
+from npu_bridge.estimator.npu.npu_loss_scale_optimizer import NPULossScaleOptimizer
+
+from npu_bridge.estimator.npu.npu_loss_scale_manager import FixedLossScaleManager
+from npu_bridge.estimator.npu.npu_loss_scale_manager import ExponentialUpdateLossScaleManager
+
 from npu_bridge.estimator import npu_ops
+from npu_bridge.estimator.npu import npu_rnn
+from npu_bridge.estimator.npu import npu_scope
+from npu_bridge.estimator.npu import util
+from npu_bridge.estimator.npu import keras_to_npu
 from npu_bridge.estimator.npu_unary_ops import npu_unary_ops
+
+from npu_bridge.hccl import hccl_ops
+
 from tensorflow.core.protobuf import rewriter_config_pb2
 from tensorflow.core.protobuf.rewriter_config_pb2 import RewriterConfig
-from hccl.manage.api import get_local_rank_id
+
+from hccl.manage.api import create_group
+from hccl.manage.api import destroy_group
 from hccl.manage.api import get_rank_size
+from hccl.manage.api import get_local_rank_size
 from hccl.manage.api import get_rank_id
+from hccl.manage.api import get_local_rank_id
+from hccl.manage.api import get_world_rank_from_group_rank
+from hccl.manage.api import get_group_rank_from_world_rank
+
+from hccl.split.api import set_split_strategy_by_idx
+from hccl.split.api import set_split_strategy_by_size
+
