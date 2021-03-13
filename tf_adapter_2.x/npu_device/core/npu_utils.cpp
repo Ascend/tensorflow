440e053050a53c70fa193ba4313eef06f61ea005
medivh-x <xp15250981628@outlook.com>
2021-03-13 19:00:03 +0800

    remove adapter 2.x on r1.3.0

--- tf_adapter_2.x/npu_device/core/npu_utils.cpp
+++
@@ -1,6 +0,0 @@
-/**
-* Copyright (c) Huawei Technologies Co., Ltd. 2021. All rights reserved.
-* Description: Common depends and micro defines for and only for data preprocess module
-*/
-
-#include "npu_utils.h"
