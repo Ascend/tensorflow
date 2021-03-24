43e07bc9d7dd1be18a92a9f4d6dfe7a55bf6e400
huanruizhi <huanruizhi@hisilicon.com>
2021-03-24 15:33:35 +0800

    aoe tuning api

---
+++ inc/toolchain/tuning_tool/aoe_types.h
@@ -0,0 +1,38 @@
+/**
+ * @file aoe_types.h
+ *
+ * Copyright (c) Huawei Technologies Co., Ltd. 2020-2020. All rights reserved.\n
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n
+ * 描述：mstune调优接口头文件
+ */
+/** @defgroup mstune mstune调优接口 */
+#ifndef AOE_TYPES_H
+#define AOE_TYPES_H
+
+#include <vector>
+#include <memory>
+#include "graph/graph.h"
+
+using AoeStatus = int32_t;
+const AoeStatus AOE_SUCCESS = 0;
+const AoeStatus AOE_FALLURE = -1;
+
+/**
+ * @ingroup mstune
+ *
+ * mstune status
+ */
+enum MsTuneStatus {
+    MSTUNE_SUCCESS,  /** tune success */
+    MSTUNE_FAILED,   /** tune failed */
+};
+
+// Option key: for train options sets
+const std::string MSTUNE_SELF_KEY = "mstune";
+const std::string MSTUNE_GEINIT_KEY = "initialize";
+const std::string MSTUNE_GESESS_KEY = "session";
+
+#endif
--- inc/toolchain/tuning_tool/tune_api.h
+++ inc/toolchain/tuning_tool/tune_api.h
@@ -11,29 +11,13 @@
 /** @defgroup mstune mstune调优接口 */
 #ifndef TUNE_API_H
 #define TUNE_API_H
-#include <vector>
 #include <map>
 #include <string>
-#include "graph/graph.h"
 #include "ge/ge_api.h"
+#include "aoe_types.h"
 
 /**
- * @ingroup mstune
- *
- * mstune status
- */
-enum MsTuneStatus {
-    MSTUNE_SUCCESS,  /** tune success */
-    MSTUNE_FAILED,   /** tune failed */
-};
-
-// Option key: for train options sets
-const std::string MSTUNE_SELF_KEY = "mstune";
-const std::string MSTUNE_GEINIT_KEY = "initialize";
-const std::string MSTUNE_GESESS_KEY = "session";
-
-/**
- * @ingroup mstune
+ * @ingroup aoe
  * @par 描述: 命令行调优
  *
  * @attention 无
@@ -46,11 +30,11 @@ const std::string MSTUNE_GESESS_KEY = "session";
  * @li tune_api.h：该接口声明所在的头文件。
  * @see 无
  * @since
- */
-MsTuneStatus MsTuning(const std::map<std::string, std::string> &option, std::string &msg);
+*/
+AoeStatus AoeOfflineTuning(const std::map<std::string, std::string> &option, std::string &msg);
 
 /**
- * @ingroup mstune
+ * @ingroup aoe
  * @par 描述: 梯度调优
  *
  * @attention 无
@@ -69,4 +53,55 @@ MsTuneStatus MsTuning(const std::map<std::string, std::string> &option, std::str
 extern "C" MsTuneStatus MsTrainTuning(ge::Graph &tuningGraph, std::vector<ge::Graph> &dependGraph,
     ge::Session *session, const std::map<std::string, std::map<std::string, std::string>> &option);
 
+/**
+ * @ingroup aoe
+ * @par 描述: 调优初始化
+ *
+ * @attention 无
+ * @param  session [IN] ge链接会话
+ * @param  option [IN] 参数集
+ * @retval #AOE_SUCCESS 执行成功
+ * @retval #AOE_FAILED 执行失败
+ * @par 依赖:
+ * @li tune_api.cpp：该接口所属的开发包。
+ * @li tune_api.h：该接口声明所在的头文件。
+ * @see 无
+ * @since
+ */
+extern "C" AoeStatus AoeOnlineInitialize(ge::Session *session, const std::map<std::string, std::string> &option);
+
+/**
+ * @ingroup aoe
+ * @par 描述: 调优去初始化
+ *
+ * @attention 无
+ * @retval #AOE_SUCCESS 执行成功
+ * @retval #AOE_FAILED 执行失败
+ * @par 依赖:
+ * @li tune_api.cpp：该接口所属的开发包。
+ * @li tune_api.h：该接口声明所在的头文件。
+ * @see 无
+ * @since
+ */
+extern "C" AoeStatus AoeOlineFinalize();
+
+/**
+ * @ingroup aoe
+ * @par 描述: 调优处理
+ *
+ * @attention 无
+ * @param  tuningGraph [IN] 调优图
+ * @param  dependGraph [IN] 调优依赖图
+ * @param  session [IN] ge连接会话
+ * @param  option [IN] 参数集. 包含调优参数及ge参数
+ * @retval #AOE_SUCCESS 执行成功
+ * @retval #AOE_FAILED 执行失败
+ * @par 依赖:
+ * @li tune_api.cpp：该接口所属的开发包。
+ * @li tune_api.h：该接口声明所在的头文件。
+ * @see 无
+ * @since
+ */
+extern "C" MsTuneStatus AoeOnlineTuning(ge::Graph &tuningGraph, std::vector<ge::Graph> &dependGraph,
+    ge::Session *session, const std::map<std::string, std::string> &option);
 #endif
