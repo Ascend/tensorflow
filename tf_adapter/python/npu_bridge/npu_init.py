a30468cb4bf1579ef19dfb93c3d93cfe1a91a99a
lianghuikang <505519763@qq.com>
2021-03-31 15:57:08 +0800

    修改set_experimental_options默认参数

--- tf_adapter/python/npu_bridge/npu_init.py
+++ tf_adapter/python/npu_bridge/npu_init.py
@@ -48,15 +48,15 @@ import atexit
 atexit.register(npu_close)
 
 experimental_options = {
-    "disable_model_pruning": True,
-    "function_optimization": RewriterConfig.OFF,
-    "constant_folding": RewriterConfig.OFF,
-    "shape_optimization": RewriterConfig.OFF,
-    "arithmetic_optimization": RewriterConfig.OFF,
-    "loop_optimization": RewriterConfig.OFF,
-    "dependency_optimization": RewriterConfig.OFF,
-    "layout_optimizer": RewriterConfig.OFF,
-    "memory_optimization": RewriterConfig.OFF
+    "disable_model_pruning": False,
+    "function_optimization": RewriterConfig.ON,
+    "constant_folding": RewriterConfig.ON,
+    "shape_optimization": RewriterConfig.ON,
+    "arithmetic_optimization": RewriterConfig.ON,
+    "loop_optimization": RewriterConfig.ON,
+    "dependency_optimization": RewriterConfig.ON,
+    "layout_optimizer": RewriterConfig.ON,
+    "memory_optimization": RewriterConfig.ON
 }
 
 def npu_hooks_append(hooks_list=[]):
