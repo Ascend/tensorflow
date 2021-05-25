582c1a60f13a8510f5fd473cef7b257509aeab7d
medivh-x <xp15250981628@outlook.com>
2021-05-25 16:40:16 +0800

    change default precision mode to allow_fp32_to_fp16

--- tf_adapter_2.x/python/npu_device/configs/npu_config.py
+++ tf_adapter_2.x/python/npu_device/configs/npu_config.py
@@ -13,7 +13,7 @@ class NpuConfig(NpuBaseConfig):
         self.variable_format_optimize = OptionValue(True, [True, False])
         self.enable_scope_fusion_passes = OptionValue(None, None)
         self.fusion_switch_file = OptionValue(None, None)
-        self.precision_mode = OptionValue('allow_mix_precision',
+        self.precision_mode = OptionValue('allow_fp32_to_fp16',
                                           ['force_fp32', 'allow_fp32_to_fp16', 'force_fp16', 'must_keep_origin_dtype',
                                            'allow_mix_precision'])
         self.auto_tune_mode = OptionValue(None, None)
