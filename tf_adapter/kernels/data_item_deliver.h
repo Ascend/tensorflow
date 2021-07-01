1a99e4949d52276939d998375900f56908b544e7
xuming <xuming20@huawei.com>
2021-07-01 21:09:59 +0800

    init server_fd_ and solve compile warning

--- tf_adapter/kernels/data_item_deliver.h
+++ tf_adapter/kernels/data_item_deliver.h
@@ -469,9 +469,9 @@ Status DataItemDeliver::CreateSockAddr(struct sockaddr_un &sock_addr,
                                        const char *path, int device_id) {
   sock_addr.sun_family = AF_UNIX;
   int len = 0;
-  if (-1 ==
-      (len = snprintf_s(sock_addr.sun_path, sizeof(sock_addr.sun_path), "%s%s%d",
-                      path, channel_name_.c_str(), device_id))) {
+  if (-1 == (len = snprintf_s(sock_addr.sun_path, sizeof(sock_addr.sun_path),
+                              sizeof(sock_addr.sun_path) - 1, "%s%s%d", path,
+                              channel_name_.c_str(), device_id))) {
     ADP_LOG(ERROR) << "Set sun_path failed.";
     LOG(ERROR) << "Set sun_path failed.";
     return errors::Internal("Set sun_path failed.");
