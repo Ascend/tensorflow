5d5456fe22c9575480d9d9ad2fb098a19a1a598c
lilianlin <lilianlin@huawei.com>
2021-07-05 16:29:22 +0800

    [fix] fix hccl op bugs.

--- tf_adapter/tests/ut/kernels/testcase/hccl_op_test.cc
+++ tf_adapter/tests/ut/kernels/testcase/hccl_op_test.cc
@@ -43,6 +43,25 @@ TEST(HcclOpTest, TestHcomAllGatherShapeInference) {
   ASSERT_EQ("[24,4]", c.DebugString(c.output(0)));
 }
 
+TEST(HcclOpTest, TestHcomAllGatherShapeInferenceInvaildRankSize) {
+  const OpRegistrationData* reg;
+  TF_CHECK_OK(OpRegistry::Global()->LookUp("HcomAllGather", &reg));
+  OpDef op_def = reg->op_def;
+  NodeDef def;
+  TF_CHECK_OK(NodeDefBuilder("dummy", &op_def)
+                  .Attr("T", DT_FLOAT)
+                  .Attr("rank_size", 0)
+                  .Attr("group", "hccl_world_group")
+                  .Input(FakeInputStub(DT_FLOAT))
+                  .Finalize(&def));
+  shape_inference::InferenceContext c(0, &def, op_def, {S({3, 4})}, {}, {}, {});
+  std::vector<shape_inference::ShapeHandle> input_shapes;
+  TF_CHECK_OK(c.input("input", &input_shapes));
+  ASSERT_EQ("[3,4]", c.DebugString(input_shapes[0]));
+  Status status = reg->shape_inference_fn(&c);
+  EXPECT_TRUE(!status.ok());
+}
+
 TEST(HcclOpTest, TestHcomReduceScatterShapeInference) {
   const OpRegistrationData* reg;
   TF_CHECK_OK(OpRegistry::Global()->LookUp("HcomReduceScatter", &reg));
@@ -61,6 +80,26 @@ TEST(HcclOpTest, TestHcomReduceScatterShapeInference) {
   ASSERT_EQ("[24,4]", c.DebugString(input_shapes[0]));
   TF_CHECK_OK(reg->shape_inference_fn(&c));
   ASSERT_EQ("[3,4]", c.DebugString(c.output(0)));
+}
+
+TEST(HcclOpTest, TestHcomReduceScatterShapeInferenceInvaildRankSize) {
+  const OpRegistrationData* reg;
+  TF_CHECK_OK(OpRegistry::Global()->LookUp("HcomReduceScatter", &reg));
+  OpDef op_def = reg->op_def;
+  NodeDef def;
+  TF_CHECK_OK(NodeDefBuilder("dummy", &op_def)
+                  .Attr("T", DT_FLOAT)
+                  .Attr("rank_size", 0)
+                  .Attr("group", "hccl_world_group")
+                  .Attr("reduction", "sum")
+                  .Input(FakeInputStub(DT_FLOAT))
+                  .Finalize(&def));
+  shape_inference::InferenceContext c(0, &def, op_def, {S({24, 4})}, {}, {}, {});
+  std::vector<shape_inference::ShapeHandle> input_shapes;
+  TF_CHECK_OK(c.input("input", &input_shapes));
+  ASSERT_EQ("[24,4]", c.DebugString(input_shapes[0]));
+  Status status = reg->shape_inference_fn(&c);
+  EXPECT_TRUE(!status.ok());
 }
 
 }  // namespace
