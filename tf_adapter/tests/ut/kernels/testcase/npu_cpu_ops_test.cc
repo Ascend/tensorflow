370c21e8281f2fe2418f19e042e244d6cd68cd7a
guopeian <guopeian1@hisilicon.com>
2021-07-09 19:49:01 +0800

    fix

---
+++ tf_adapter/tests/ut/kernels/testcase/npu_cpu_ops_test.cc
@@ -0,0 +1,29 @@
+#include <memory>
+#include "tf_adapter/kernels/npu_cpu_ops.cc"
+#include "gtest/gtest.h"
+
+namespace tensorflow {
+class NpuCpuOpTest : public testing::Test {
+ protected:
+  virtual void SetUp() {}
+  virtual void TearDown() {}
+};
+
+TEST_F(NpuCpuOpTest, TestCacheAdd) {
+    DataTypeSlice input_types({DT_RESOURCE, DT_INT64});
+    MemoryTypeSlice input_memory_types;
+    DataTypeSlice output_types({DT_INT64, DT_INT64, DT_INT64, DT_INT64});
+    MemoryTypeSlice output_memory_types;
+    DeviceBase *device = new DeviceBase(Env::Default());
+    NodeDef *node_def = new NodeDef();
+    OpDef *op_def = new OpDef();
+    OpKernelConstruction *context = new OpKernelConstruction(DEVICE_CPU, device, nullptr, node_def, op_def, nullptr,
+                                                             input_types, input_memory_types, output_types, output_memory_types,
+                                                             1, nullptr);
+    CacheAddOp cache(context);
+    delete device;
+    delete node_def;
+    delete op_def;
+    delete context;
+}
+}
