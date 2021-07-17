acc3ce00fc10f28db3c9d4c84acdb8af8eff64e0
yuxing <yuxing11@hisilicon.com>
2021-07-17 15:15:32 +0800

    add env AOE_MODE

--- tf_adapter/util/npu_attrs.cc
+++ tf_adapter/util/npu_attrs.cc
@@ -292,21 +292,22 @@ Status GetAoeTuningConfigs(const google::protobuf::Map<std::string, AttrValue> p
   std::string aoe_mode_config;
   if (params.count("aoe_mode")) {
     aoe_mode_config = params.at("aoe_mode").s();
+    LOG(Info) << "yuxingaoe_mode_config: " << aoe_mode_config;
   }
   if (aoe_mode_config.empty() && aoe_mode_env == nullptr) {
     aoe_mode = "";
-    cout << "yuxing1: " << aoe_mode << endl; 
+    LOG(Info) << "yuxing1: " << aoe_mode;
   } else if (aoe_mode_config.empty() && aoe_mode_env != nullptr) {
     aoe_mode = aoe_mode_env;
-    cout << "yuxing2: " << aoe_mode << endl; 
+    LOG(Info) << "yuxing2: " << aoe_mode;
   } else {
     aoe_mode = aoe_mode_config;
-    cout << "yuxing3: " << aoe_mode << endl;
+    LOG(Info) << "yuxing3: " << aoe_mode;
   }
   if (!aoe_mode.empty()) {
-    cout << "yuxing4: " << aoe_mode << endl; 
+    LOG(Info) << "yuxing4: " << aoe_mode;
     Status s  = CheckAoeMode(aoe_mode);
-    cout << "yuxing5: " << aoe_mode << endl; 
+    LOG(Info) << "yuxing5: " << aoe_mode;
     if (!s.ok()) { return s; }
     if (params.count("work_path")) {
       std::string tmp_path = params.at("work_path").s();
