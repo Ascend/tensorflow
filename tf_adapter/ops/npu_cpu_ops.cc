5823e6f35c4588bc615f676f50a17886952d83ed
zhujingjing <zhujingjing6@huawei.com>
2021-08-05 16:46:30 +0800

    add scatter_elements op

--- tf_adapter/ops/npu_cpu_ops.cc
+++ tf_adapter/ops/npu_cpu_ops.cc
@@ -261,16 +261,16 @@ REGISTER_OP("DenseImageWarpGrad")
   });
 
   REGISTER_OP("ScatterElements")
-  .Input("data: T")
-  .Input("indices: indexT")
-  .Input("updates: T")
-  .Output("y: T")
-  .Attr("axis: int = 0")
-  .Attr("T: numbertype")
-  .Attr("indexT: {int32, int64}")
-  .SetShapeFn([](shape_inference::InferenceContext *c) {
-    auto data_shape = c->input(0);
-    c->set_output(0, data_shape);
-    return Status::OK();
-  });
+    .Input("data: T")
+    .Input("indices: indexT")
+    .Input("updates: T")
+    .Output("y: T")
+    .Attr("axis: int = 0")
+    .Attr("T: numbertype")
+    .Attr("indexT: {int32, int64}")
+    .SetShapeFn([](shape_inference::InferenceContext *c) {
+      auto data_shape = c->input(0);
+      c->set_output(0, data_shape);
+      return Status::OK();
+    });
 }  // namespace tensorflow
