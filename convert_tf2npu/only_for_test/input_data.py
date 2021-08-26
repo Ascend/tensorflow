55cb7cf45c0b28e3ee30522aeeeb462873ce9b35
hanxiaoxuan <hanxiaoxuan@hisilicon.com>
2021-08-26 14:26:08 +0800

    add -d arg in main_win

---
+++ convert_tf2npu/only_for_test/input_data.py
@@ -0,0 +1,119 @@
+Module(
+|   body=[
+|   |   Expr(
+|   |   |   value=Str(
+|   |   |   |   s='FunctionsfordownloadingandreadingMNISTdata.'
+|   |   |   )
+|   |   ),
+|   |   ImportFrom(
+|   |   |   module='__future__',
+|   |   |   names=[
+|   |   |   |   alias(
+|   |   |   |   |   name='absolute_import',
+|   |   |   |   |   asname=None
+|   |   |   |   )
+|   |   |   ],
+|   |   |   level=0
+|   |   ),
+|   |   ImportFrom(
+|   |   |   module='__future__',
+|   |   |   names=[
+|   |   |   |   alias(
+|   |   |   |   |   name='division',
+|   |   |   |   |   asname=None
+|   |   |   |   )
+|   |   |   ],
+|   |   |   level=0
+|   |   ),
+|   |   ImportFrom(
+|   |   |   module='__future__',
+|   |   |   names=[
+|   |   |   |   alias(
+|   |   |   |   |   name='print_function',
+|   |   |   |   |   asname=None
+|   |   |   |   )
+|   |   |   ],
+|   |   |   level=0
+|   |   ),
+|   |   ImportFrom(
+|   |   |   module='npu_bridge.npu_init',
+|   |   |   names=[
+|   |   |   |   alias(
+|   |   |   |   |   name='*',
+|   |   |   |   |   asname=None
+|   |   |   |   )
+|   |   |   ],
+|   |   |   level=0
+|   |   ),
+|   |   Import(
+|   |   |   names=[
+|   |   |   |   alias(
+|   |   |   |   |   name='gzip',
+|   |   |   |   |   asname=None
+|   |   |   |   )
+|   |   |   ]
+|   |   ),
+|   |   Import(
+|   |   |   names=[
+|   |   |   |   alias(
+|   |   |   |   |   name='os',
+|   |   |   |   |   asname=None
+|   |   |   |   )
+|   |   |   ]
+|   |   ),
+|   |   Import(
+|   |   |   names=[
+|   |   |   |   alias(
+|   |   |   |   |   name='tempfile',
+|   |   |   |   |   asname=None
+|   |   |   |   )
+|   |   |   ]
+|   |   ),
+|   |   Import(
+|   |   |   names=[
+|   |   |   |   alias(
+|   |   |   |   |   name='numpy',
+|   |   |   |   |   asname=None
+|   |   |   |   )
+|   |   |   ]
+|   |   ),
+|   |   ImportFrom(
+|   |   |   module='six.moves',
+|   |   |   names=[
+|   |   |   |   alias(
+|   |   |   |   |   name='urllib',
+|   |   |   |   |   asname=None
+|   |   |   |   )
+|   |   |   ],
+|   |   |   level=0
+|   |   ),
+|   |   ImportFrom(
+|   |   |   module='six.moves',
+|   |   |   names=[
+|   |   |   |   alias(
+|   |   |   |   |   name='xrange',
+|   |   |   |   |   asname=None
+|   |   |   |   )
+|   |   |   ],
+|   |   |   level=0
+|   |   ),
+|   |   Import(
+|   |   |   names=[
+|   |   |   |   alias(
+|   |   |   |   |   name='tensorflow',
+|   |   |   |   |   asname='tf'
+|   |   |   |   )
+|   |   |   ]
+|   |   ),
+|   |   ImportFrom(
+|   |   |   module='tensorflow.contrib.learn.python.learn.datasets.mnist',
+|   |   |   names=[
+|   |   |   |   alias(
+|   |   |   |   |   name='read_data_sets',
+|   |   |   |   |   asname=None
+|   |   |   |   )
+|   |   |   ],
+|   |   |   level=0
+|   |   )
+|   ]
+)
