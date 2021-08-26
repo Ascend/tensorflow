276d3f21d12e5a3a07aa16d71678b9bc157a5c1c
yangyang016 <yangyang306@hisilicon.com>
2021-08-26 10:46:53 +0800

    fix comments

--- tf_adapter/kernels/k_means_centroids.cc
+++ tf_adapter/kernels/k_means_centroids.cc
@@ -11,7 +11,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 
-Copyright (C) 2019-2020. Huawei Technologies Co., Ltd. All rights reserved.
+Copyright (C) 2021. Huawei Technologies Co., Ltd. All rights reserved.
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
