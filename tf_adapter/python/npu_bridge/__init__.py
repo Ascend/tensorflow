c1a3995c3aaad21dc3d35e9739185cf24df383c4
hanxiaoxuan <hanxiaoxuan@hisilicon.com>
2021-09-17 14:23:32 +0800

    fix error caused by keras dropout

--- tf_adapter/python/npu_bridge/__init__.py
+++ tf_adapter/python/npu_bridge/__init__.py
@@ -21,7 +21,6 @@ from npu_bridge.helper import helper
 from npu_bridge.estimator.npu import npu_estimator
 from npu_bridge.hccl import hccl_ops
 from npu_bridge.estimator.npu.npu_plugin import npu_close
-from npu_bridge.estimator import npu_ops
 import atexit
 atexit.register(npu_close)
 __all__ = [_s for _s in dir() if not _s.startswith('_')]
