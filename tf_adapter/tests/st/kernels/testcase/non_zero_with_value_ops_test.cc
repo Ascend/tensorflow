9c5965c2ddf87b9935ff9198880742af5b7a160b
yuanbin_22 <yuanbin22@huawei.com>
2021-10-26 19:19:23 +0800

    增加测试用例

---
+++ tf_adapter/tests/st/kernels/testcase/non_zero_with_value_ops_test.cc
@@ -0,0 +1,32 @@
+#include <memory>
+#include "tf_adapter/kernels/non_zero_with_value_ops.cc"
+#include "gtest/gtest.h"
+
+namespace tensorflow {
+class NonZeroWithValueOpTest : public testing::Test {
+ protected:
+  virtual void SetUp() {}
+  virtual void TearDown() {}
+};
+
+TEST_F(NonZeroWithValueTest, TestNonZeroWithValue) {
+    DataTypeSlice input_types({DT_INT32});
+    MemoryTypeSlice input_memory_types;
+    DataTypeSlice output_types({DT_INT64});
+    MemoryTypeSlice output_memory_types;
+    DeviceBase *device = new DeviceBase(Env::Default());
+    NodeDef *node_def = new NodeDef();
+    OpDef *op_def = new OpDef();
+    OpKernelConstruction *context = new OpKernelConstruction(DEVICE_CPU, device, nullptr, node_def, op_def, nullptr,
+                                                             input_types, input_memory_types, output_types, output_memory_types,
+                                                             1, nullptr);
+    NonZeroWithValueOP<int> non_zero_with_value(context);
+    OpKernelContext *ctx = nullptr;
+    non_zero_with_value.Compute(ctx);
+    non_zero_with_value.IsExpensive();
+    delete device;
+    delete node_def;
+    delete op_def;
+    delete context;
+}
+}
