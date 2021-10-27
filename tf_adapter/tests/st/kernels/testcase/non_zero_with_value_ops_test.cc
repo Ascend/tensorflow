1601593da85bdfbbcfa6c78b6d18bf1d9c5f6875
yuanbin_22 <yuanbin22@huawei.com>
2021-10-27 10:26:20 +0800

    增加测试用例

--- tf_adapter/tests/st/kernels/testcase/non_zero_with_value_ops_test.cc
+++ tf_adapter/tests/st/kernels/testcase/non_zero_with_value_ops_test.cc
@@ -1,6 +1,14 @@
 #include <memory>
 #include "tf_adapter/kernels/non_zero_with_value_ops.cc"
 #include "gtest/gtest.h"
+#include "tensorflow/core/framework/attr_value.pb.h"
+#include "tensorflow/core/framework/attr_value_util.h"
+#include "tensorflow/core/framework/fake_input.h"
+#include "tensorflow/core/framework/node_def.pb.h"
+#include "tensorflow/core/framework/node_def_builder.h"
+#include "tensorflow/core/framework/op_kernel.h"
+#include "tensorflow/core/framework/shape_inference.h"
+#include "tensorflow/core/platform/test.h"
 
 namespace tensorflow {
 class NonZeroWithValueOpTest : public testing::Test {
@@ -8,6 +16,20 @@ class NonZeroWithValueOpTest : public testing::Test {
   virtual void SetUp() {}
   virtual void TearDown() {}
 };
+
+PartialTensorShape TShape(std::initializer_list<int64> dims) {
+  return PartialTensorShape(dims);
+}
+
+FakeInputFunctor FakeInputStub(DataType dt) {
+  return [dt](const OpDef& op_def, int in_index, const NodeDef& node_def,
+              NodeDefBuilder* builder) {
+    char c = 'a' + (in_index % 26);
+    string in_node =  string(&c, 1);
+    builder->Input(in_node, 0, dt);
+    return Status::OK();
+  };
+}
 
 TEST_F(NonZeroWithValueOpTest, TestNonZeroWithValue) {
     DataTypeSlice input_types({DT_INT32});
