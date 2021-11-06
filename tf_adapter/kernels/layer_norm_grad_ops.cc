284c6865ffd74cbf84963b200eeba93e8a190ee9
root <lixulong@huawei.com>
2021-11-06 10:36:30 +0800

    add tf api

--- tf_adapter/kernels/layer_norm_grad_ops.cc
+++ tf_adapter/kernels/layer_norm_grad_ops.cc
@@ -34,6 +34,6 @@ public:
     return false; }
 };
 
-REGISTER_KERNEL_BUILDER(Name("LayerNormGrad").Device(DEVICE_CPU), LayerNormGradOp);
+REGISTER_KERNEL_BUILDER(Name("LayerNormGrad").Device(DEVICE_CPU), LayerNormGradOp<float>);
 }  // namespace tensorflow
 
