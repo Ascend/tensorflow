a95486e5362813d412c2c184b830bdd3d0a9530e
unknown <sunmaocheng@huawei.com>
2021-12-06 15:50:11 +0800

    add Utest Stest

---
+++ tf_adapter/tests/ut/kernels/testcase/fast_gelu_v2_ops_test.cc
@@ -0,0 +1,67 @@
+#include <memory>
+#include "tf_adapter/kernels/aicore/fast_gelu_v2_ops.cc"
+#include "gtest/gtest.h"
+#include "tensorflow/core/framework/attr_value.pb.h"
+#include "tensorflow/core/framework/attr_value_util.h"
+#include "tensorflow/core/framework/fake_input.h"
+#include "tensorflow/core/framework/node_def.pb.h"
+#include "tensorflow/core/framework/node_def_builder.h"
+#include "tensorflow/core/framework/op_kernel.h"
+#include "tensorflow/core/framework/shape_inference.h"
+#include "tensorflow/core/platform/test.h"
+#include <iostream>
+namespace tensorflow {
+namespace {
+
+PartialTensorShape TShape(std::initializer_list<int64> dims) {
+  return PartialTensorShape(dims);
+}
+
+FakeInputFunctor FakeInputStub(DataType dt) {
+  return [dt](const OpDef &op_def, int in_index, const NodeDef &node_def,
+              NodeDefBuilder *builder) {
+    char c = 'a' + (in_index % 26);
+    string in_node = string(&c, 1);
+    builder->Input(in_node, 0, dt);
+    return Status::OK();
+  };
+}
+
+TEST(FastGeluV2OpTest, TestFastGeluV2) {
+  DataTypeSlice input_types({DT_FLOAT});
+  MemoryTypeSlice input_memory_types;
+  DataTypeSlice output_types({DT_FLOAT});
+  MemoryTypeSlice output_memory_types;
+  DeviceBase *device = new DeviceBase(Env::Default());
+  NodeDef *node_def = new NodeDef();
+  OpDef *op_def = new OpDef();
+  OpKernelConstruction *context = new OpKernelConstruction(
+      DEVICE_CPU, device, nullptr, node_def, op_def, nullptr, input_types,
+      input_memory_types, output_types, output_memory_types, 1, nullptr);
+  FastGeluV2Op fast_gelu_v2(context);
+  OpKernelContext *ctx = nullptr;
+  fast_gelu_v2.Compute(ctx);
+  fast_gelu_v2.IsExpensive();
+  delete device;
+  delete node_def;
+  delete op_def;
+  delete context;
+}
+
+TEST(FastGeluV2OpTest, TestFastGeluV2ShapeInference) {
+  const OpRegistrationData *reg;
+  TF_CHECK_OK(OpRegistry::Global()->LookUp("FastGeluV2", &reg));
+  OpDef op_def = reg->op_def;
+  NodeDef def;
+  TF_CHECK_OK(NodeDefBuilder("dummy", &op_def)
+                  .Attr("T", DT_FLOAT)
+                  .Input(FakeInputStub(DT_FLOAT))
+                  .Finalize(&def));
+  shape_inference::InferenceContext c(0, &def, op_def, {TShape({16, 32})},
+                                      {});
+  std::vector<shape_inference::ShapeHandle> input_shapes;
+  TF_CHECK_OK(reg->shape_inference_fn(&c));
+  ASSERT_EQ("[16,32]", c.DebugString(c.output(0)));
+}
+} // namespace
+} // namespace tensorflow
