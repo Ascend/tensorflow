f67bfc03bfebd2a350770a132dbd2c82eea5a672
huanruizhi <huanruizhi@hisilicon.com>
2021-12-10 18:39:54 +0800

    bugfix unary

--- tf_adapter/optimizers/om_partition_subgraphs_pass.h
+++ tf_adapter/optimizers/om_partition_subgraphs_pass.h
@@ -58,6 +58,7 @@ class OMPartitionSubgraphsPass : public GraphOptimizationPass {
                             std::map<std::string, std::string> &graph_options);
   Status ProcessGetNext(Node *node, std::string enable_dp,
                         std::vector<Node*> &remove_nodes, Graph *graphIn);
+  Status SplitUnaryOpsComposition(Graph *graph, Node *node);
 };
 }  // namespace tensorflow
 #endif  // TENSORFLOW_OM_PARTITION_SUBGRAPHS_PASS_H_
