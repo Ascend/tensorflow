bc3fba973346111d624a1d5658a6ad9d0c831cc4
liuwei00382665 <lovline.liuwei@huawei.com>
2021-12-24 21:34:30 +0800

    fix codecheck warning

--- tf_adapter/python/npu_bridge/npu_cpu/lru_cache.py
+++ tf_adapter/python/npu_bridge/npu_cpu/lru_cache.py
@@ -22,15 +22,15 @@ gen_npu_cpu_ops = helper.get_gen_ops()
 
 
 class LruCache(object):
-    """lru cache class"""
+    """ Lru cache class. """
     def __init__(self, cache_size=100000, load_factor=1, dtype=tf.uint32):
         self._cache_size = cache_size
         self._load_factor = load_factor
         self._dtype = dtype
         self._cache = gen_npu_cpu_ops.lru_cache(
-            cache_size = self._cache_size,
-            load_factor = self._load_factor,
-            dtype = self._dtype
+            cache_size=self._cache_size,
+            load_factor=self._load_factor,
+            dtype=self._dtype
         )
 
     ## 提供CacheAdd功能
@@ -38,10 +38,10 @@ class LruCache(object):
     # @param ids int类型，输入索引
     # @return result 对ids执行完swap in/swap out操作后输出的索引张量
     def cache_add(self, ids):
-        """add caches"""
+        """ Add caches. """
         result = gen_npu_cpu_ops.cache_add(
-            cache = self._cache,
-            ids = ids
+            cache=self._cache,
+            ids=ids
         )
         return result
 
@@ -50,10 +50,10 @@ class LruCache(object):
     # @param ids int类型，输入索引
     # return result 输入ids对应cache中的索引
     def cache_remote_index_to_local(self, ids):
-        """cahce remote index to local"""
+        """ Cahce remote index to local. """
         result = gen_npu_cpu_ops.cache_remote_index_to_local(
-            cache = self._cache,
-            ids = ids
+            cache=self._cache,
+            ids=ids
         )
         return result
 
@@ -61,9 +61,11 @@ class LruCache(object):
     # @param cache resource类型，保存lruCache资源
     # return result 输出cache中所有的id
     def cache_all_index_to_local(self):
-        """cache all index to local"""
+        """
+        cache all index to local
+        """
         result = gen_npu_cpu_ops.cache_all_index_to_local(
-            cache = self._cache,
-            dtype = self._dtype
+            cache=self._cache,
+            dtype=self._dtype
         )
         return result
