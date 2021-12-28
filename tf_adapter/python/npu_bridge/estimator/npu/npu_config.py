c808a781749bc4d23b56c99f807c2dc34066dad6
hanxiaoxuan <hanxiaoxuan@hisilicon.com>
2021-12-28 08:26:36 +0000

    update tf_adapter/python/npu_bridge/estimator/npu/npu_config.py.

--- tf_adapter/python/npu_bridge/estimator/npu/npu_config.py
+++ tf_adapter/python/npu_bridge/estimator/npu/npu_config.py
@@ -251,7 +251,7 @@ class NPURunConfig(run_config_lib.RunConfig):
         return dump_config
 
     def _get_horovod_mode(self, horovod_mode):
-        if isinstance(horovod_mode, bool) is False:
+        if not isinstance(horovod_mode, bool):
             raise ValueError('"horovod_mode" type must be bool')
         return horovod_mode
 
