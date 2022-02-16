6dcc40b0610dd1c2c8736f70d206b672d45b3221
liuxianxk <liuxiankun2@h-partners.com>
2022-02-16 17:45:53 +0800

    misra clean

--- tf_adapter/util/npu_attrs.cc
+++ tf_adapter/util/npu_attrs.cc
@@ -181,7 +181,7 @@ inline Status checkDumpStep(const std::string &dump_step) {
 
 inline Status checkDumpMode(const std::string &dump_mode) {
   std::set<string> dump_mode_list = {"input", "output", "all"};
-  if ((std::set<string>::const_iterator iter = dump_mode_list.find(dump_mode)) != dump_mode_list.end()) {
+  if ((std::set<string>::const_iterator iter = dump_mode_list.find(dump_mode)) != dump_mode_list.cend()) {
     return Status::OK();
   } else {
     return errors::InvalidArgument("dump mode should be one of the list:[input, output, all]");
