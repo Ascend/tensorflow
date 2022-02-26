bc0096913fc1df844fcfcd6c1a843bb520391cd3
liyefeng803@huawei.com <liyefeng803@huawei.com>
2022-02-26 16:25:59 +0800

    test

--- tf_adapter/tests/ut/kernels/testcase/n_p_u_get_float_statusV2_test.cc
+++ tf_adapter/tests/ut/kernels/testcase/n_p_u_get_float_statusV2_test.cc
@@ -73,11 +73,11 @@ TEST(NPUGetFloatStatusV2OpTest, TestNPUGetFloatStatusV2OShapeInference) {
   OpDef op_def = reg->op_def;
   NodeDef def;
   TF_CHECK_OK(NodeDefBuilder("dummy", &op_def)
-                  .Input(FakeInputStub(DT_FLOAT))
+                  .Input([FakeInputStub(DT_FLOAT), FakeInputStub(DT_FLOAT)])
                   .Attr("T", DT_FLOAT)
-                  .Attr("N", 5)
+                  .Attr("N", 2)
                   .Finalize(&def));
-  shape_inference::InferenceContext c(0, &def, op_def, {[TShape({8})]}, {}, {}, {});
+  shape_inference::InferenceContext c(0, &def, op_def, {[TShape({8}), TShape({8})]}, {}, {}, {});
   std::vector<shape_inference::ShapeHandle> input_shapes;
   TF_CHECK_OK(reg->shape_inference_fn(&c));
   ASSERT_EQ("[8]", c.DebugString(c.output(0)));
