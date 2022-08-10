39e12a22ed0ff30302e0058188be4669b44320af
王涛 <wangtao123@huawei.com>
2022-08-10 12:12:03 +0000

    !1467 update json version Merge pull request !1467 from 王涛/ge_dev

--- tf_adapter_2.x/cmake/nlohmann_json.cmake
+++ tf_adapter_2.x/cmake/nlohmann_json.cmake
@@ -3,20 +3,18 @@ if (DEFINED ASCEND_INSTALLED_PATH)
     if (TF_PKG_SERVER)
         FetchContent_Declare(
                 nlohmann_json
-                URL ${TF_PKG_SERVER}/libs/json/v3.6.1/include.zip
-                URL_HASH MD5=0dc903888211db3a0f170304cd9f3a89
+                URL https://ascend-cann.obs.myhuaweicloud.com/json/repository/archive/json-v3.10.1.zip
         )
     else ()
         FetchContent_Declare(
                 nlohmann_json
-                URL https://ascend-cann.obs.myhuaweicloud.com/json/v3.6.1/include.zip
-                URL_HASH MD5=0dc903888211db3a0f170304cd9f3a89
+                URL https://ascend-cann.obs.myhuaweicloud.com/json/repository/archive/json-v3.10.1.zip
         )
     endif ()
     FetchContent_GetProperties(nlohmann_json)
     if (NOT nlohmann_json_POPULATED)
         FetchContent_Populate(nlohmann_json)
-        include_directories(${nlohmann_json_SOURCE_DIR})
+        include_directories(${nlohmann_json_SOURCE_DIR}/include)
     endif ()
 else ()
     include_directories(${ASCEND_CI_BUILD_DIR}/third_party/json/include)
